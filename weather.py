92a415b266d12a62973431c35480744d242559c6
Udhay Karthik R <udhaykarthik51@gmail.com>
2025-02-11 12:59:43 +0530

    Changed units to metric for Celsius temperature

--- weather.py
+++ weather.py
@@ -8,7 +8,8 @@ load_dotenv()
 
 def get_current_weather(city="Coimbatore city"):
 
-    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+
 
     weather_data = requests.get(request_url).json()
 
